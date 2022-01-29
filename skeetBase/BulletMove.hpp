6565be32423b2cf2fb18f27faf4cf60b9f810263
Josh Spendlove <joshvspendlove@gmail.com>
2022-01-28 20:28:43 -0700

    Added BulletMove, it's subclasses, and it's implementation into the Bullet class and sub-classes.

---
+++ skeetBase/BulletMove.hpp
@@ -0,0 +1,13 @@
+//
+//  BulletMove.hpp
+//  Lab03
+//
+//  Created by Josh Spendlove on 1/27/22.
+//
+
+#ifndef BulletMove_hpp
+#define BulletMove_hpp
+
+#include <stdio.h>
+
+#endif /* BulletMove_hpp */
